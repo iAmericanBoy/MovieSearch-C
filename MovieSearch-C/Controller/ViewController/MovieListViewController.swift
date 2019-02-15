b6732a6bc11f94a5d52f383c5ff488cb5d87b627
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-15 10:15:07 -0700

    Build out storyboard

---
+++ MovieSearch-C/Controller/ViewController/MovieListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MovieListViewController.swift
+//  MovieSearch-C
+//
+//  Created by Dominic Lanzillotta on 2/15/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
