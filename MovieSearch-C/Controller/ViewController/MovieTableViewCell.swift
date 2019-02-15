15b8e79b27d14f5e3349f2e74c81f048a907443f
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-15 10:20:27 -0700

    Adds Outlets

---
+++ MovieSearch-C/Controller/ViewController/MovieTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MoviewTableViewCell.swift
+//  MovieSearch-C
+//
+//  Created by Dominic Lanzillotta on 2/15/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+    //MARK: - Outlets
+    @IBOutlet weak var movieImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    //MARK: - Properties
+    
+}
