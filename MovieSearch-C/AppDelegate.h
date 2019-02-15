94aa70bbf4aad22b12ac74877f0792601eac9736
iAmericanBoy <32912858+iAmericanBoy@users.noreply.github.com>
2019-02-15 09:11:01 -0700

    Initial Commit

---
+++ MovieSearch-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieSearch-C
+//
+//  Created by Dominic Lanzillotta on 2/15/19.
+//  Copyright © 2019 Dominic Lanzillotta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
